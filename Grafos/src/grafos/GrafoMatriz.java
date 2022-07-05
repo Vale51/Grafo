e7b8d6932d2d8c13f5e6c1c022839c7568790561
Vale51 <102689997+Vale51@users.noreply.github.com>
2022-07-05 17:41:55 -0300

    carga de archivos

---
+++ Grafos/src/grafos/GrafoMatriz.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grafos;
+
+/**
+ *
+ * @author user
+ */
+public class GrafoMatriz {
+    int numVerts;
+    static int MaxVerts = 20;
+    Vertice [] verts;
+    int [][] matAd;
+    
+    public GrafoMatriz()
+    {
+    this(MaxVerts);
+    }
+    
+    public GrafoMatriz(int mx)
+    {
+        matAd = new int [mx][mx];
+        verts = new Vertice[mx];
+        for (int i = 0; i < mx; i++)
+            for (int j = 0; j < mx; j++)
+                matAd[i][j] = 0;
+        numVerts = 0;        
+    }
+    
+    public void nuevoVertice (String nom)
+    {
+        boolean esta = numVertice(nom) >= 0;
+        if (!esta)
+        {
+            Vertice v = new Vertice(nom);
+            v.asigVert(numVerts);
+            verts[numVerts++] = v;
+        }
+    }
+    
+    public int numVertice(String vs)
+    {
+        Vertice v = new Vertice(vs);
+        boolean encontrado = false;
+        int i = 0;
+        for (; (i < numVerts) && !encontrado;)
+        {
+            encontrado = verts[i].equals(v);
+            if (!encontrado) i++ ;
+        }
+        return (i < numVerts) ? i : -1 ;
+    }
+    
+    public void nuevoArco(String a, String b)throws Exception
+    {
+        int va, vb;
+        va = numVertice(a);
+        vb = numVertice(b);
+        System.out.println("va: " + va);
+        System.out.println("vb: " + vb);
+        if (va < 0 || vb < 0) throw new Exception ("Vértice no existe");
+        matAd[va][vb] = 1;
+    }
+    
+    public void nuevoArco(int va, int vb)throws Exception
+    {
+        if (va < 0 || vb < 0) throw new Exception ("Vértice no existe");
+        matAd[va][vb] = 1;
+    }
+    
+    public boolean adyacente(String a, String b)throws Exception
+    {
+        int va, vb;
+        va = numVertice(a);
+        vb = numVertice(b);
+        
+        if (va < 0 || vb < 0) throw new Exception ("Vértice no existe");
+            return matAd[va][vb] == 1;
+    }
+    
+    public boolean adyacente(int va, int vb)throws Exception
+    {
+        if (va < 0 || vb < 0) throw new Exception ("Vértice no existe");
+        return matAd[va][vb] == 1;
+    }
+    
+    
+    public void mostrarMatriz(){
+        for (int i = 0; i < this.numVerts; i++){
+            for (int j = 0; j < this.numVerts; j++){
+                System.out.print(matAd[i][j]); 
+            }
+            System.out.println("");
+        }               
+        
+    }
+    
+}
---
+++ Grafos/src/grafos/Grafos.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grafos;
+
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Grafos {
+
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int menu = 1, salir = 1;
+        String nombre;
+        
+        
+        System.out.println("Ingrese la cantidad de vertices:");
+        GrafoMatriz grafo = new GrafoMatriz(sc.nextInt());
+        sc.nextLine();
+    
+        while(menu!=0){
+            System.out.println("Elija una opcion:");
+            System.out.println("1.Ingresar vertice");
+            System.out.println("2.Agregar arco");
+            System.out.println("3.Imprimir matriz");
+            System.out.println("0.Salir");
+            menu = sc.nextInt();
+            sc.nextLine();
+            switch(menu){
+                case 1:
+                    salir = 1;
+                    while(salir != 0){
+                        System.out.println("Ingrese el nombre del vertice:");
+                        nombre = sc.nextLine();
+                        grafo.nuevoVertice(nombre);
+                        System.out.println("Desea seguir agregando vertices?");
+                        System.out.println("1.Si");
+                        System.out.println("0.No");
+                        salir = sc.nextInt();
+                        sc.nextLine();
+                    }
+                    break;
+                    
+                case 2:
+                    salir = 1;
+                    while(salir != 0){
+                        System.out.println("Ingrese el nombre de dos vertices:");
+                        try {
+                            grafo.nuevoArco(sc.nextLine(), sc.nextLine());
+                        } catch (Exception ex) {
+                            System.out.println("No existen los vertices ingresados");
+                        }
+                        System.out.println("Desea seguir agregando arcos?");
+                        System.out.println("1.Si");
+                        System.out.println("0.No");
+                        salir = sc.nextInt();
+                        sc.nextLine();
+                    }
+                    break;
+                   
+                case 3:
+                    grafo.mostrarMatriz();
+                    
+                    break;
+                    
+            }
+            
+        }
+        
+    }
+    
+}
